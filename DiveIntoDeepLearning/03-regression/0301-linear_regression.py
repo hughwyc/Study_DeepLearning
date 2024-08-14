3c8b0e7939d4af4ed22302dc3a80f0088289a01f
hugh <hugh.wyc@gmail.com>
2024-08-14 10:45:17 +0800

    240814 update

--- DiveIntoDeepLearning/03-regression/0301-linear_regression.py
+++ DiveIntoDeepLearning/03-regression/0301-linear_regression.py
@@ -14,6 +14,8 @@
 # 深度学习实践者很少会去花费大力气寻找这样一组参数，使得在训练集上的损失达到最小。
 # 事实上，更难做到的是找到一组参数，这组参数能够在我们从未见过的数据上实现较低的损失， 这一挑战被称为泛化（generalization）。
 
+# 在高斯噪声的假设下，最小化均方误差等价于对线性模型的极大似然估计。
+
 import math
 import numpy as np
 import torch
@@ -32,3 +34,18 @@ print(f'{timer.stop():.5f} sec')
 timer.start()
 d = a + b
 print(f'{timer.stop():.5f} sec')
+
+
+def normal(x, mu, sigma):
+    p = 1 / math.sqrt(2 * math.pi * sigma ** 2)
+    return p * np.exp(-0.5 / sigma ** 2 * (x - mu) ** 2)
+
+
+# 再次使用numpy进行可视化
+x = np.arange(-7, 7, 0.01)
+
+# 均值和标准差对
+params = [(0, 1), (0, 2), (3, 1)]
+plot(x, [normal(x, mu, sigma) for mu, sigma in params], xlabel='x',
+     ylabel='p(x)', figsize=(10, 6),
+     legend=[f'mean {mu}, std {sigma}' for mu, sigma in params])
